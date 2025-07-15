3cd2f6e4af030f7de99a73eef595ea4f3b749d07
Harshitha BR <Harshitha BR@LAPTOP-9GK0BTJQ>
2025-07-15 11:12:59 +0530

    Add project files.

---
+++ LeaveManagementSystem1.Web/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using LeaveManagementSystem1.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem1.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateOfBirth = new DateTime(1954,12,09)
+            };
+            return View(data);
+        }
+
+        public IActionResult More()
+        {
+            var data = new MoreViewModel
+            {
+                Age = 25,
+                Address = "Baburayana Koppal"
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem1.Web/Models/MoreViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementSystem1.Web.Models
+{
+    public class MoreViewModel
+    {
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+    }
+}
